5352910f2272800a597c8d56a517cf383f065b7e
Matthias Hellmann <hellmann@uni-koeln.de>
2025-04-07 20:09:21 +0200

    load bzip module and add libxkbfile to LD_LIBRARY_PATH

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+module load tools/bzip2/1.0.8-GCCcore-12.3.0
+
+export XKBFILE=$PWD/libxkbfile/install/lib64
 export XFONT2=$PWD/libxfont/install/lib
-export XKBDIR=$PWD
+
+export XKBDIR=$PWD/xkbdata/xkb
 export XKB_BINDIR=$PWD/xkbcomp
-LD_LIBRARY_PATH=$XFONT2:$LD_LIBRARY_PATH $PWD/xserver/install/bin/Xvfb :99
+#export XKB_OUTPUT_DIR=$XKBDIR
+
+#LD_LIBRARY_PATH=$XKBFILE:$XFONT2:$LD_LIBRARY_PATH $PWD/xserver/install/bin/Xvfb :99 -nolisten tcp
+LD_LIBRARY_PATH=$XKBFILE:$XFONT2:$LD_LIBRARY_PATH $PWD/xserver/install/bin/Xvfb :99
 
