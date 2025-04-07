b1cc7b046d64bcdaac5d8d1a18bc9f46c66ecfba
Matthias Hellmann <hellmann@uni-koeln.de>
2025-04-07 21:31:44 +0200

    modular build script

--- build-script.sh
+++ build-script.sh
@@ -1,99 +1,151 @@
 #!/bin/bash
 
+CLONE_MACROS=1
+CLONE_XORGPROTO=1
+CLONE_LIBXKBFILE=1
+CLONE_XKBCOMP=1
+CLONE_UTIL=1
+CLONE_LIBFONTENC=1
+CLONE_LIBXTRANS=1
+CLONE_LIBXFONT=1
+CLONE_LIBXCVT=1
+CLONE_XSERVER=1
 
-git clone https://gitlab.freedesktop.org/xorg/util/macros.git
-git clone https://gitlab.freedesktop.org/xorg/app/xkbcomp.git
-git clone https://gitlab.freedesktop.org/xorg/proto/xorgproto.git
-git clone https://gitlab.freedesktop.org/xorg/lib/libxkbfile.git
-git clone https://gitlab.freedesktop.org/xorg/font/util.git
-git clone https://gitlab.freedesktop.org/xorg/lib/libfontenc.git
-git clone https://gitlab.freedesktop.org/xorg/lib/libxfont.git
-git clone https://gitlab.freedesktop.org/xorg/lib/libxcvt.git
-git clone https://gitlab.freedesktop.org/xorg/lib/libxtrans.git
-git clone https://gitlab.freedesktop.org/xorg/xserver.git
+APPLY_XSERVER_PATCH=1
+
+BUILD_MACROS=1
+BUILD_XORGPROTO=1
+BUILD_LIBXKBFILE=1
+BUILD_XKBCOMP=1
+BUILD_UTIL=1
+BUILD_LIBFONTENC=1
+BUILD_LIBXTRANS=1
+BUILD_LIBXFONT=1
+BUILD_LIBXCVT=1
+BUILD_XSERVER=1
+
+##############################
+
+[ "$CLONE_MACROS"       -eq 1 ] && git clone https://gitlab.freedesktop.org/xorg/util/macros.git
+[ "$CLONE_XORGPROTO"    -eq 1 ] && git clone https://gitlab.freedesktop.org/xorg/proto/xorgproto.git
+[ "$CLONE_LIBXKBFILE"   -eq 1 ] && git clone https://gitlab.freedesktop.org/xorg/lib/libxkbfile.git
+[ "$CLONE_XKBCOMP"      -eq 1 ] && git clone https://gitlab.freedesktop.org/xorg/app/xkbcomp.git
+[ "$CLONE_UTIL"         -eq 1 ] && git clone https://gitlab.freedesktop.org/xorg/font/util.git
+[ "$CLONE_LIBFONTENC"   -eq 1 ] && git clone https://gitlab.freedesktop.org/xorg/lib/libfontenc.git
+[ "$CLONE_LIBXTRANS"    -eq 1 ] && git clone https://gitlab.freedesktop.org/xorg/lib/libxtrans.git
+[ "$CLONE_LIBXFONT"     -eq 1 ] && git clone https://gitlab.freedesktop.org/xorg/lib/libxfont.git
+[ "$CLONE_LIBXCVT"      -eq 1 ] && git clone https://gitlab.freedesktop.org/xorg/lib/libxcvt.git
+[ "$CLONE_XSERVER"      -eq 1 ] && git clone https://gitlab.freedesktop.org/xorg/xserver.git
 
 pushd macros
-./autogen.sh
-./configure --prefix=$PWD/install
-make -j10
-make install
+if [ "$BUILD_MACROS" -eq 1 ]; then
+    ./autogen.sh
+    ./configure --prefix=$PWD/install
+    make -j10
+    make install
+fi
 export MACROS=$PWD/install/share/pkgconfig:$PWD/install/share/aclocal
 popd
 
 pushd xorgproto
-meson setup build --prefix=$PWD/install
-pushd build
-ninja
-ninja install
-popd
+if [ "$BUILD_XORGPROTO" -eq 1 ]; then
+    meson setup build --prefix=$PWD/install
+    pushd build
+    ninja
+    ninja install
+    popd
+fi
 export XORGPROTO=$PWD/install/share/pkgconfig
 popd
 
 pushd libxkbfile
-meson setup build --prefix=$PWD/install
-pushd build
-ninja
-ninja install
-popd
+if [ "$BUILD_LIBXKBFILE" -eq 1 ]; then
+    meson setup build --prefix=$PWD/install
+    pushd build
+    ninja
+    ninja install
+    popd
+fi
 export LIBXKBFILE=$PWD/install/lib64/pkgconfig
 popd
 
 pushd xkbcomp
-ACLOCAL_PATH=$MACROS:$LIBXKBFILE:$XORGPROTO:$ACLOCAL_PATH PKG_CONFIG_PATH=$MACROS:$LIBXKBFILE:$XORGPROTO:PKG_CONFIG_PATH ./autogen.sh
-./configure --prefix=$PWD/install
-make -j10
-make install
+if [ "$BUILD_XKBCOMP" -eq 1 ]; then
+    DEPS="$MACROS:$LIBXKBFILE:$XORGPROTO"
+    ACLOCAL_PATH="$DEPS:$ACLOCAL_PATH" PKG_CONFIG_PATH="$DEPS:$PKG_CONFIG_PATH" ./autogen.sh
+    ./configure --prefix=$PWD/install
+    make -j10
+    make install
+fi
 export XKBCOMP=$PWD/install/lib/pkgconfig
 popd
 
 pushd util
-./autogen.sh
-./configure --prefix=$PWD/install
-make -j10
-make install
+if [ "$BUILD_UTIL" -eq 1 ]; then
+    ./autogen.sh
+    ./configure --prefix=$PWD/install
+    make -j10
+    make install
+fi
 export UTIL=$PWD/install/lib/pkgconfig:$PWD/install/share/aclocal
 popd
 
 pushd libfontenc
-ACLOCAL_PATH=$MACROS:$UTIL:$ACLOCAL_PATH PKG_CONFIG_PATH=$MACROS:$UTIL:$PKG_CONFIG_PATH ./autogen.sh
-./configure --prefix=$PWD/install
-make -j10
-make install
+if [ "$BUILD_LIBFONTENC" -eq 1 ]; then
+    DEPS="$MACROS:$UTIL"
+    ACLOCAL_PATH="$DEPS:$ACLOCAL_PATH" PKG_CONFIG_PATH="$DEPS:$PKG_CONFIG_PATH" ./autogen.sh
+    ./configure --prefix=$PWD/install
+    make -j10
+    make install
+fi
 export LIBFONTENC=$PWD/install/lib/pkgconfig:$PWD/install/share/aclocal
 popd
 
 pushd libxtrans
-ACLOCAL_PATH=$MACROS:$ACLOCAL_PATH PKG_CONFIG_PATH=$MACROS:$PKG_CONFIG_PATH ./autogen.sh
-./configure --prefix=$PWD/install
-make -j10
-make install
+if [ "$BUILD_LIBXTRANS" -eq 1 ]; then
+    DEPS="$MACROS"
+    ACLOCAL_PATH="$DEPS:$ACLOCAL_PATH" PKG_CONFIG_PATH="$DEPS:$PKG_CONFIG_PATH" ./autogen.sh
+    ./configure --prefix=$PWD/install
+    make -j10
+    make install
+fi
 export LIBXTRANS=$PWD/install/share/pkgconfig:$PWD/install/share/aclocal:$PWD
 popd
 
 pushd libxfont
-ACLOCAL_PATH=$MACROS:$LIBFONTENC:$LIBXTRANS:$UTIL:$ACLOCAL_PATH PKG_CONFIG_PATH=$MACROS:$LIBFONTENC:$LIBXTRANS:$UTIL:$PKG_CONFIG_PATH ./autogen.sh
-./configure --prefix=$PWD/install
-make -j10
-make install
+if [ "$BUILD_LIBXFONT" -eq 1 ]; then
+    DEPS="$MACROS:$LIBFONTENC:$LIBXTRANS:$UTIL"
+    ACLOCAL_PATH="$DEPS:$ACLOCAL_PATH" PKG_CONFIG_PATH="$DEPS:$PKG_CONFIG_PATH" ./autogen.sh
+    ./configure --prefix=$PWD/install
+    make -j10
+    make install
+fi
 export LIBXFONT=$PWD/install/lib/pkgconfig
 popd
 
 pushd libxcvt
-meson setup build --prefix="$PWD/install"
-pushd build
-ninja
-ninja install
-popd
+if [ "$BUILD_LIBXCVT" -eq 1 ]; then
+    meson setup build --prefix="$PWD/install"
+    pushd build
+    ninja
+    ninja install
+    popd
+fi
 export LIBXCVT=$PWD/install/lib64/pkgconfig
 popd
 
-#cp default_xkb.patch ./xserver/default_xkb.patch
 pushd xserver
-#git apply default_xkb.patch
-PKG_CONFIG_PATH="$XORGPROTO:$LIBXKBFILE:$LIBXFONT:$LIBFONTENC:$LIBXCVT:$LIBXTRANS:$XKBCOMP:$PKG_CONFIG_PATH" meson setup build --reconfigure -Dudev=false -Dudev_kms=false --prefix=$PWD/install
-pushd build
-ninja
-ninja install
-popd
+if [ "$APPLY_XSERVER_PATCH" -eq 1 ]; then
+    cp ../default_xkb.patch ./default_xkb.patch
+    git apply default_xkb.patch
+fi
+if [ "$BUILD_XSERVER" -eq 1 ]; then
+    DEPS="$XORGPROTO:$LIBXKBFILE:$LIBXFONT:$LIBFONTENC:$LIBXCVT:$LIBXTRANS:$XKBCOMP"
+    PKG_CONFIG_PATH="$DEPS:$PKG_CONFIG_PATH" meson setup build --reconfigure -Dudev=false -Dudev_kms=false -Dglx=true --prefix="$PWD/install"
+    pushd build
+    ninja
+    ninja install
+    popd
+fi
 popd
 
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 module load tools/bzip2/1.0.8-GCCcore-12.3.0
+module load vis/Mesa/23.1.4-GCCcore-12.3.0
 
 export XKBFILE=$PWD/libxkbfile/install/lib64
 export XFONT2=$PWD/libxfont/install/lib
@@ -8,5 +9,6 @@ export XFONT2=$PWD/libxfont/install/lib
 export XKBDIR=$PWD/xkbdata/xkb
 export XKB_BINDIR=$PWD/xkbcomp
 
-LD_LIBRARY_PATH=$XKBFILE:$XFONT2:$LD_LIBRARY_PATH $PWD/xserver/install/bin/Xvfb :99 -nolisten tcp
+LD_LIBRARY_PATH=$XKBFILE:$XFONT2:$LD_LIBRARY_PATH $PWD/xserver/install/bin/Xvfb :99 -nolisten tcp +extension GLX +extension RENDER +extension DOUBLE-BUFFER
 
+#env DISPLAY=:99 vglrun glxinfo
